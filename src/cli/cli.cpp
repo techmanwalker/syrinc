4bdb1f7f68df2a753826030c08a7755f94cc3227
Lito Parra <lito.15@proton.me>
2025-12-10 20:42:05 -0700

    Guard against user-defined offset zero

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -4,7 +4,6 @@
 
 #include "debug.hpp"
 #include "modules/lrcfilerewriter.hpp"
-#include "modules/timestampconv.hpp"
 #include "modules/tokens.hpp"
 
 int main(int argc, char** argv)
@@ -13,7 +12,8 @@ int main(int argc, char** argv)
 
     opt.add_options()
         ("f,file",      "input .lrc file",  cxxopts::value<std::string>())
-        ("o,offset", "override offset, in ms", cxxopts::value<std::vector<std::string>>())
+        ("o,offset", "override offset, in ms",
+        cxxopts::value<long>()->default_value(std::to_string(LONG_MIN))) // sentinel value
         ("i,invert",    "invert offset sign")
         ("d,drop-metadata", "Drop out all metadata tags")
         ("h,help",      "print usage");
@@ -28,15 +28,13 @@ int main(int argc, char** argv)
 
     /* ----- JSON-like retrieval ----- */
     std::string file         = result["file"].as<std::string>();
-    long        offset       = 0; // will retrieve just next
     bool        invert       = result["invert"].as<bool>();
     bool        dropmetadata = result["drop-metadata"].as<bool>();
 
-    if (result.count("offset")) {
-        const auto& v = result["offset"].as<std::vector<std::string>>();
-        if (!v.empty() && !v[0].empty() && is_numeric_only(v[0]))
-            offset = std::stol(v[0]);
-    }
+    // retrieve offset like this so we can detect if the user typed -o 0 by accident
+    long raw_offset = result["offset"].as<long>();
+    bool offset_provided = raw_offset != LONG_MIN;
+    long offset = offset_provided ? raw_offset : 0;
 
     // options that will be fed to the process_lyrics engine
 
@@ -48,6 +46,11 @@ int main(int argc, char** argv)
 
     // For debugging
     LOG(options, "Processing lyrics with following options");
+
+    // Fire a warning if the user manually typed offset 0
+    if (offset_provided && offset == 0)
+        std::clog << "warning: -o 0 means \"use file offset\"; "
+                 "file offset will be used.\n";
 
     if (!file.empty())
         std::cout <<
